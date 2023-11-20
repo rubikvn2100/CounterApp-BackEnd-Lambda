e96455fd6ebd2c907609a7d09126d0265b493b04
Khoa Le <rubikvn2100@gmail.com>
2023-11-19 21:20:10 -0800

    Update "setup_and_test.sh" script for coverage testing and HTML report generation.

--- setup_and_test.sh
+++ setup_and_test.sh
@@ -1,16 +1,65 @@
 #!/bin/bash
 
-python3 -m venv venv
+export PYTHONPATH="$(pwd)/src/api_handler:$PYTHONPATH"
 
+coverage_test=false
+html_report=false
+
+usage() {
+  echo "Usage: $0 [options]"
+  echo "Options:"
+  echo "  --coverage   Run coverage tests"
+  echo "  --html       Generate an HTML coverage report (requires --coverage)"
+  echo "  --help       Display this help message"
+  exit 1
+}
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --coverage)
+      coverage_test=true
+      ;;
+    --html)
+      html_report=true
+      ;;
+    --help)
+      usage
+      ;;
+    *)
+      echo "Unknown option: $1"
+      usage
+      ;;
+  esac
+  shift
+done
+
+python3 -m venv venv
 source venv/bin/activate
 
 pip3 install black pytest boto3 moto
 
 black src/ tests/
 
-export PYTHONPATH="$(pwd)/src/api_handler:$PYTHONPATH"
+if [ "$coverage_test" = true ]; then
+  pip3 install coverage
 
-if pytest; then
+  coverage run -m pytest
+
+  if [ "$html_report" = true ]; then
+    coverage html
+
+    open htmlcov/index.html
+    if [ $? -ne 0 ]; then
+      echo "HTML report cannot be opened. Please open \"htmlcov/index.html\" manually."
+    fi
+  else
+    coverage report
+  fi
+else
+  pytest
+fi
+
+if [ $? -eq 0 ]; then
   deactivate
   exit 0
 else
