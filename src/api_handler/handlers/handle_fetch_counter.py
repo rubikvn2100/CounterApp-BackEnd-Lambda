69dd0c96b49d0d23598899147867f8faaa24cf0f
Khoa Le <rubikvn2100@gmail.com>
2023-11-19 20:45:56 -0800

    Implement the "handle_fetch_counter" and add corresponding tests.

--- src/api_handler/handlers/handle_fetch_counter.py
+++ src/api_handler/handlers/handle_fetch_counter.py
@@ -1,5 +1,12 @@
 import json
+from session_management import SessionManager
 
 
 def handle_fetch_counter(event: dict, context) -> dict:
-    return {"statusCode": 200, "body": json.dumps("Response for GET /api/counter")}
+    session_manager = SessionManager(event)
+
+    validation_result = session_manager.validate_session()
+    if validation_result["statusCode"] != 200:
+        return validation_result
+
+    return session_manager.fetch_counter()
--- tests/api_handler/handlers/test_handle_fetch_counter.py
+++ tests/api_handler/handlers/test_handle_fetch_counter.py
@@ -1,15 +1,29 @@
 import json
+import os
 from src.api_handler.handlers.handle_fetch_counter import handle_fetch_counter
+from test_util import create_valid_session_token, set_counter
 
 
-def test_placeholder_fetch_counter():
+def test_handle_fetch_counter_validate_session_failed():
     event = {}
     context = {}
-    expected_response = {
-        "statusCode": 200,
-        "body": json.dumps("Response for GET /api/counter"),
-    }
 
     response = handle_fetch_counter(event, context)
 
-    assert response == expected_response
+    assert response["statusCode"] != 200
+
+
+def test_handle_fetch_counter_successful():
+    counter = 100
+    set_counter(counter)
+
+    token = create_valid_session_token()
+
+    event = {"headers": {"Authorization": f"Bearer {token}"}}
+    context = {}
+
+    response = handle_fetch_counter(event, context)
+    response_body = json.loads(response["body"])
+
+    assert response["statusCode"] == 200
+    assert response_body["counter"] == counter
