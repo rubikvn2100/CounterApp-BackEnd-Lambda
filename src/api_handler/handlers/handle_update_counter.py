a075d5287954e03866c5fc2edce260d3435b90c1
Khoa Le <rubikvn2100@gmail.com>
2023-11-19 21:12:34 -0800

    Implement the handle_update_counter and add corresponding tests.

--- src/api_handler/handlers/handle_update_counter.py
+++ src/api_handler/handlers/handle_update_counter.py
@@ -1,5 +1,18 @@
 import json
+from session_management import SessionManager
 
 
 def handle_update_counter(event: dict, context) -> dict:
-    return {"statusCode": 200, "body": json.dumps("Response for POST /api/counter")}
+    session_manager = SessionManager(event)
+
+    validation_result = session_manager.validate_session()
+    if validation_result["statusCode"] != 200:
+        return validation_result
+
+    validation_result = session_manager.validate_user_activity()
+    if validation_result["statusCode"] != 200:
+        return validation_result
+
+    session_manager.update_counter()
+
+    return session_manager.fetch_counter()
--- tests/api_handler/handlers/test_handle_update_counter.py
+++ tests/api_handler/handlers/test_handle_update_counter.py
@@ -1,15 +1,50 @@
 import json
 from src.api_handler.handlers.handle_update_counter import handle_update_counter
+from test_util import create_valid_session_token, set_counter
+from util import get_current_time
 
 
-def test_placeholder_update_counter():
+def test_handle_update_counter_validate_session_failed():
     event = {}
     context = {}
-    expected_response = {
-        "statusCode": 200,
-        "body": json.dumps("Response for POST /api/counter"),
-    }
 
     response = handle_update_counter(event, context)
 
-    assert response == expected_response
+    assert response["statusCode"] != 200
+
+
+def test_handle_update_counter_validate_user_activity_failed():
+    token = create_valid_session_token()
+
+    event = {"headers": {"Authorization": f"Bearer {token}"}}
+    context = {}
+
+    response = handle_update_counter(event, context)
+
+    assert response["statusCode"] != 200
+
+
+def test_handle_update_counter_successful():
+    counter = 100
+    set_counter(counter)
+
+    token = create_valid_session_token()
+
+    current_time = float(get_current_time())
+    timestamps = []
+
+    N = 5
+    for i in range(1, N + 1):
+        timestamps.append(current_time + 0.15 * i)
+
+    event = {
+        "headers": {"Authorization": f"Bearer {token}"},
+        "body": json.dumps({"timestamps": timestamps}),
+    }
+    context = {}
+
+    response = handle_update_counter(event, context)
+    response_body = json.loads(response["body"])
+
+    assert response["statusCode"] == 200
+    assert response_body["counter"] == counter + N
